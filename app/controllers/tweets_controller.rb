b1cee04ebd36a656840eb16758d00c8c74bdaba0
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-06-11 10:59:53 +0900

    createアクション記述

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,7 +5,23 @@ class TweetsController < ApplicationController
   end
 
   def new
-    @tweet = TweetTag.new
+    @tweet = TweetsTag.new
   end
-  
+
+  def create
+    @tweet = TweetsTag.new(tweet_params)
+    if @tweet.valid?
+      @tweet.save
+      return redirect_to root_path
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweets_tag).permit(:message, :name)
+  end
+
 end
