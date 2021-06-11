0fcaf150163d77c7b9c0a556cba1aef3b356c9c1
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-06-11 12:52:45 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'tweets#index'
   resources :tweets, only: [:index, :new, :create] do
     collection do
-      get 'serach'
+      get 'search'
     end
   end
 end
